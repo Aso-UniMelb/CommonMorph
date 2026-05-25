54da955bd847a3eae16cb74ec582b5a4d3acf249
aso-mehmudi <aso.mehmudi@gmail.com>
2026-05-26 00:46:47 +1000

    video+paper

--- wwwroot/js/tutorials.js
+++ wwwroot/js/tutorials.js
@@ -2,6 +2,9 @@ let tutorials = {
   welcome: {
     title: 'Welcome to the Common Morph!',
     content: `<p>This tutorial will guide you through the process of contributing to the Common Morph project.</p>
+<div style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; background: #000; border-radius: 8px; margin: 20px 0; box-shadow: 0 4px 12px rgba(0,0,0,0.15);">
+  <iframe src="https://www.youtube.com/embed/KrbE2jhGVZ0" title="CommonMorph Video Tutorial" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; border: 0;" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+</div>
 <p>Table of Contents:</p>
 <ul id="toc"></ul>
 `,
